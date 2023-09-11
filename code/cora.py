03bd6f51f10afea0358503ba6316cdf6ee27b691
Ishaan Gupta <ishaan.msg@gmail.com>
2023-09-11 12:23:06 -0500

    Cora Refactor

--- code/cora.py
+++ code/cora.py
@@ -1,5 +1,6 @@
-
 import os
+import time
+import statistics
 import csv
 import collections
 import numpy as np
@@ -14,144 +15,20 @@ from torch_geometric.utils import to_networkx
 from torch_geometric.datasets import Planetoid
 import networkx as nx
 from networkx.algorithms import community
+
 import openai
 import random
+from utils import draw_graph,plot_label_distribution
+from metrics import compute_accuracy,record_metrics
+from response_parser import parse_response
+from connection_information import generate_edgelist,generate_node_label_dict,generate_textual_edgelist,generate_textual_edgelist2,generate_graphlist,generate_graphlist_constrained,edge_list_to_adjacency_list
+from prompt_generation import get_completion,generate_text_for_prompt
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
-
-def draw_graph(edge_example):
-    node_example = np.unique(edge_example.flatten())
-    plt.figure(figsize=(10, 6))
-    G = nx.Graph()
-    G.add_nodes_from(node_example)
-    G.add_edges_from(list(zip(edge_example[0], edge_example[1])))
-    nx.draw_networkx(G, with_labels=False)
-    plt.show()
-
-def plot_label_distribution(data):
-    counter = collections.Counter(data.y.numpy())
-    counter = dict(counter)
-    print(counter)
-    count = [x[1] for x in sorted(counter.items())]
-    plt.figure(figsize=(10, 6))
-    plt.bar(range(7), count)
-    plt.xlabel("class", size=20)
-    plt.show()
-
-def print_dataset_stats(dataset):
-    data = dataset[0]
-    print(f'Number of nodes: {data.num_nodes}')
-    # Number of nodes: 2708
-    print(f'Number of edges: {data.num_edges}')
-    # Number of edges: 10556
-    print(f'Number of features: {data.num_node_features}')
-    # Number of edge features: 0
-    print(f'Number of classes: {dataset.num_classes}')
-    # Number of classes: 7
-    print(f'Has isolated nodes: {data.has_isolated_nodes()}')  # False
-    print(f'Has self-loops: {data.has_self_loops()}')  # False
-    print(f'Is undirected: {data.is_undirected()}')  # True
 
 def load_dataset(data_dir, dataset_name):
     dataset = Planetoid(root=data_dir, name=dataset_name)
     return dataset
 
-
-def generate_graphlist(num_nodes_to_sample,no_of_hops,data):
-    # List to store sampled graphs
-    graph_list = []
-    #choose some random nodes to sample
-    sampled_indices = torch.randint(0, data.num_nodes, (num_nodes_to_sample,))
-    # Convert the PyG graph to NetworkX graph
-    nx_graph = to_networkx(data, to_undirected=True)
-
-    # Convert sampled indices to integers, list of center nodes
-    nx_ids = [int(node_id.item()) for node_id in sampled_indices]
-
-    # Sample first 10 nodes and create a graph centered around each node -- modify this to be random!
-    for center_node in nx_ids:
-        sampled_subgraph = nx.ego_graph(nx_graph, center_node, radius=no_of_hops, undirected=True)
-        graph_list.append(sampled_subgraph)
-    return graph_list
-
-def generate_edgelist(graph):
-    # Print out the labels associated with a graph
-    edge_list = list(graph.edges())
-    return edge_list
-
-def generate_textual_edgelist(edge_list):           
-        # Convert the edge list information into text
-        edgelist_converted = ''
-        for edge in edge_list:
-            source, target = edge
-            edgelist_converted += f'Node {source} is connected to Node {target}. '
-        return edgelist_converted
-
-def generate_node_label_dict(graph, node_with_question_mark): 
-    node_label_dict= {} # node: label
-    for node in graph.nodes():
-        if node == node_with_question_mark:
-            ground_truth = data.y[node].item()
-            label = "?"
-        else:
-            label = data.y[node].item()  # Extract node label
-        node_label_dict[node]=label
-    return ground_truth, node_label_dict
-        
-def generate_text_for_prompt(i, graph, edge_text_flag):
-    text = ""
-    ground_truth = ""
-    text+= f"Graph {i+1}"+"\n"
-    edge_list = generate_edgelist(graph)
-    if edge_text_flag:
-        edge_list_converted = generate_textual_edgelist(edge_list)
-        text+="Edgelist: "+str(edge_list_converted)+"\n"
-    else:
-        text+="Edgelist: "+str(edge_list)+"\n"
-    # Randomly choose a node to have a "?" label
-    node_with_question_mark = random.choice(list(graph.nodes()))
-    ground_truth, node_label_dict = generate_node_label_dict(graph, node_with_question_mark)
-
-    text+=f"Node to Label Mapping : "+"\n"
-    for node in node_label_dict:
-        text+=f"Node {node}: Label {node_label_dict[node]}| "
-    return text, node_with_question_mark, ground_truth
-
-
-def parse_response(response, delimiter):
-    try:
-        start_index = response.index(delimiter) + len(delimiter)
-        value = response[start_index:].strip()
-        return value
-    except ValueError:
-        return None
-
-
-def compute_accuracy(csv_filename):
-    total_count = 0
-    correct_count = 0
-
-    with open(csv_filename, 'r') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-        for row in csv_reader:
-            total_count += 1
-            ground_truth = row['GroundTruth']
-            parsed_value = row['Parsed Value']
-            if ground_truth == parsed_value:
-                correct_count += 1
-
-    if total_count == 0:
-        return 0
-    else:
-        accuracy = correct_count / total_count
-        return accuracy
 
 if __name__== '__main__':  
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -174,55 +51,139 @@ if __name__== '__main__':
     openai.api_key = os.environ["OPENAI_API_UMNKEY"]
     
     
-    #---- PARAMS -----
-    no_of_hops = 2
-    use_edge_text = False
-    edge_format = "edgelist"
-    if use_edge_text==True:
-        edge_format = "edgetext"
-    no_of_sampled_nodes = 10
-    #--------------------
-    filename = "./results/cora/cora_"+str(no_of_sampled_nodes)+"nodes_"+str(no_of_hops)+"hop_"+edge_format+".csv"
-    graph_list = generate_graphlist(no_of_sampled_nodes,no_of_hops,data)
-    with open(filename,'w') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['GroundTruth', 'Parsed Value', 'Prompt', 'Response'])
-        for i, graph in enumerate(graph_list):
-            text, node_with_question_mark, ground_truth = generate_text_for_prompt(i, graph,use_edge_text)
-            prompt = f"""
-            Task : Node Label Prediction (Predict the label of the node marked with a ?, in the format "Label of Node = " : <predicted label>) given the edge connectivity and label information in the text enclosed in triple backticks.
-            ```{text}```
-            """
-            try:
-                response = get_completion(prompt)
-            except openai.error.APIError as e:
-                #Handle API error here, e.g. retry or log
-                print(f"OpenAI API returned an API Error: {e}")
-                pass
-            except openai.error.APIConnectionError as e:
-                #Handle connection error here
-                print(f"Failed to connect to OpenAI API: {e}")
-                pass
-            except openai.error.RateLimitError as e:
-                #Handle rate limit error (we recommend using exponential backoff)
-                print(f"OpenAI API request exceeded rate limit: {e}")
-                pass
-            print(text)
+    # ---- PARAMS --- #
+    NO_OF_HOPS = [2]
+    USE_EDGE_TEXT = [False]
+    NO_OF_SAMPLED_NODES = [50]
+    RUN_COUNT = 2
+    USE_ADJACENCY = True # in this case USE_EDGE_TEXT has to be set to false so that it generates an edgelist first.
+    #model = "gpt-3.5-turbo"
+    model = "gpt-4"
+    rate_limit_pause = 1.2 # calculated as per rate limit policy
 
-            delimiter_options = ['=', ':']  # You can add more delimiters if needed
-            parsed_value = None
-
-            for delimiter in delimiter_options:
-                parsed_value = parse_response(response, delimiter)
-                if parsed_value is not None:
-                    csv_writer.writerow([ground_truth, parsed_value, f'"{prompt}"', f'"{response}"'])
-                    break
-
-            print("RESPONSE --> ", response)
-            print("Node with ?: ", node_with_question_mark, "Label: ",ground_truth)
-            print("="*30)
-    accuracy = compute_accuracy(filename)
-    print(f"Accuracy: {accuracy:.2%}")
-
+    # ------------------
+    # this logs all the run metrics -- this needs to be changed everytime you run it
+    metrics_filename = "./results/cora/edge/edge_connection/metrics_test2.csv"
+    with open(metrics_filename, 'w') as metrics_file:
+        metrics_writer = csv.writer(metrics_file)
+        metrics_writer.writerow(["no of hops", "edgetext", "sampled nodes", "mean accuracy", "SD-accuracy","mean failure fraction","SD failure fraction"," mean token err frac", "SD token frac"])
     
-    
+    # This stores all error messages - So need to rename file for every run -- remove this bit later
+    csv_filename = "./results/cora/edge/edge_connection/invalid_request_errors_test.csv"
+    # Open the CSV file in append mode?
+    with open(csv_filename, 'w', newline='') as csvfilei:
+        csv_writer_i = csv.writer(csvfilei)
+        csv_writer_i.writerow(["setting", "nodes","edges", "error", "response if present"])
+
+        for hops in NO_OF_HOPS:
+            for use_edge in USE_EDGE_TEXT:
+                if use_edge==True:
+                    edge_format = "edgetext"
+                elif USE_ADJACENCY == True :
+                    edge_format = "adjacency"
+                else :
+                    edge_format = "edgelist"
+
+                for sampled_nodes in NO_OF_SAMPLED_NODES:
+                    acc_list = []
+                    fail_list =[]
+                    token_err_list = []
+                    for run_count in range(0,RUN_COUNT):
+                        start_time = time.time()
+                        print("Run Count : ", run_count+1)
+                        print("****Starting generation for :", hops, " hops, ",edge_format, " , ", sampled_nodes, " sample nodes****")
+                        filename = "./results/cora/edge/edge_connection/co_"+str(sampled_nodes)+"nodes_"+str(hops)+"hop_"+edge_format+"_run"+str(run_count)+".csv"
+
+                        # get the y labels and the graph list (in this dataset we need to access the y labels in a special way)
+                        y_labels_dict, nx_ids, graph_list = generate_graphlist_constrained(sampled_nodes,hops,data)
+
+                        with open(filename,'w') as csvfile:
+                            csv_writer = csv.writer(csvfile)
+                            csv_writer.writerow(['GroundTruth', 'Parsed Value', 'Prompt', 'Response'])
+                            error_count = 0
+                            token_err_count = 0
+                            for i, graph in enumerate(graph_list):
+                                #print("Graph ",i)
+                                text, node_with_question_mark, ground_truth = generate_text_for_prompt(i, nx_ids, graph, y_labels_dict, use_edge, USE_ADJACENCY)
+                                error = ""
+                                prompt = f"""
+                                Task : Node Label Prediction (Predict the label of the node marked with a ?, given the adjacency list information as a dictionary of type "node:node neighborhood"
+                                and node-label mapping in the text enclosed in triple backticks. Response should be in the format "Label of Node = <predicted label>". If the predicted label cannot be determined, return "Label of Node = -1") 
+                                ```{text}```
+                                """
+                                #prompt = f"""
+                                #Task : Node Label Prediction (Predict the label of the node marked with a ?, given the edge connectivity information and node-label mapping in the text enclosed in triple backticks. Response should be in the format "Label of Node = <predicted label>". If the predicted label cannot be determined, return "Label of Node = -1") 
+                                #```{text}```
+                                #"""
+                                try:
+                                    response = get_completion(prompt, model)
+                                except Exception as e:
+                                    error_count+=1
+                                    if error_count>5:
+                                        if isinstance(e, openai.error.RateLimitError):
+                                            raise Exception("Rate limit exceeded too many times.") from e
+                                        elif isinstance(e, openai.error.ServiceUnavailableError):
+                                            raise Exception("Service unavailable too many times.") from e
+                                        else:
+                                            raise e
+                                
+                                    if isinstance(e, openai.error.RateLimitError):
+                                        print(f"Rate limit exceeded. Pausing for {rate_limit_pause} seconds.")
+                                    elif isinstance(e, openai.error.ServiceUnavailableError):
+                                        print(f"Service unavailable; you likely paused and resumed. Pausing on our own for {rate_limit_pause} seconds to help reset things and then retrying.")
+                                    elif isinstance(e, openai.error.InvalidRequestError):
+                                        token_err_count+=1
+                                        print("Prompt tokens > context limit of 4097")
+                                        print(e)
+                                        error = str(e)                                   
+                                        csv_writer_i.writerow([f'"{(hops,edge_format,sampled_nodes)}"', f'{graph.number_of_nodes()}', f'{graph.number_of_edges()}', f'{error}'])
+                                    else:
+                                        print(f"Type of error: {type(e)}")
+                                        print(f"Error: {e}")
+                                        print(f"Pausing for {rate_limit_pause} seconds.")
+                                    time.sleep(rate_limit_pause)
+                                    continue
+                                
+                                print(text)
+                                delimiter_options = ['=', ':']  # You can add more delimiters if needed
+                                parsed_value = None
+                                for delimiter in delimiter_options: 
+                                    parsed_value = parse_response(response, delimiter) # check for better rules here!
+                                    if parsed_value is not None: # general checking for the delimiter responses
+                                        csv_writer.writerow([ground_truth, parsed_value, f'"{prompt}"', f'"{response}"'])
+                                        break
+                                    else :
+                                        print("Delimiter not found in response from the LLM")
+                                        csv_writer.writerow([ground_truth, parsed_value, f'"{prompt}"', f'"{response}"','delimiter not found']) # remove delimiter not found part later
+                                        csv_writer_i.writerow([f'"{(hops,edge_format,sampled_nodes)}"', f'{graph.number_of_nodes()}', f'{graph.number_of_edges()}','delimiter not found', f'"{response}"'])
+                                        break
+                                        
+                                print("RESPONSE --> ", response)
+                                print("Node with ?: ", node_with_question_mark, "Label: ",ground_truth)
+                                print("="*30)
+                        accuracy, failure_perc = compute_accuracy(filename) # check if value based on entries is same as sample nodes
+                        token_err_perc = token_err_count/sampled_nodes
+                        print(f"% of times LLM prompt was too large: {token_err_perc}")
+                        print(f"Accuracy: {accuracy:.2%}")
+                        print(f"No of times LLM failed to predict a label: {failure_perc:.2%}")
+                        acc_list.append(accuracy)
+                        token_err_list.append(token_err_perc)
+                        fail_list.append(failure_perc)
+
+                        #end_time = time.time()
+                        #iteration_time = end_time - start_time
+                        #print(f"Iteration took {iteration_time} seconds")
+
+                    # Record average metrics in the metrics.csv file
+                    mean_accuracy = statistics.mean(acc_list)
+                    std_accuracy = statistics.stdev(acc_list)
+                    mean_failure = statistics.mean(fail_list)
+                    std_failure = statistics.stdev(fail_list)
+                    mean_token_perc = statistics.mean(token_err_list)
+                    std_token_perc = statistics.stdev(token_err_list)
+
+                    # write the average metrics out
+                    record_metrics(metrics_filename, hops, use_edge, sampled_nodes, mean_accuracy, std_accuracy, mean_failure, std_failure, mean_token_perc, std_token_perc)
+                        
+                        
+                                        
